c96924242b75979f37dab9d95217731d2ec3bfa2
jonas-stack <jonasspeedtsberg99@gmail.com>
2023-11-07 09:55:35 +0100

    init

---
+++ src/gui/MainController.java
@@ -0,0 +1,27 @@
+package gui;
+
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class MainController {
+
+    public Button btnSearch;
+    public Button btnReadA;
+    public Button btnReadN;
+    public Button btnClearHistory;
+
+   
+
+    public void Search(ActionEvent actionEvent) {
+    }
+
+    public void ReadA(ActionEvent actionEvent) {
+    }
+
+    public void ReadN(ActionEvent actionEvent) {
+    }
+
+    public void ClearHistory(ActionEvent actionEvent) {
+    }
+}
